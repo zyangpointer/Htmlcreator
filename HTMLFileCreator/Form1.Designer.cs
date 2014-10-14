0f679b56459a100eadeb3b54ce88b67166cdc9a0
zyangpointer <zyang.pointer@gmail.com>
2014-10-14 20:09:49 +0800

    Initial Commit

---
+++ HTMLFileCreator/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace HTMLFileCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_create = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(86, 103);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(126, 117);
+            this.btn_create.TabIndex = 0;
+            this.btn_create.Text = "Create";
+            this.btn_create.UseVisualStyleBackColor = true;
+            this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 307);
+            this.Controls.Add(this.btn_create);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_create;
+    }
+}
+
