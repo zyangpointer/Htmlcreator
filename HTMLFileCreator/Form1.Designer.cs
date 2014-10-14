84d46e71eb6baac27126ea8074aaeef8741ef4f5
zyangpointer <zyang.pointer@gmail.com>
2014-10-14 21:14:34 +0800

    Add Choosing Directory

--- HTMLFileCreator/Form1.Designer.cs
+++ HTMLFileCreator/Form1.Designer.cs
@@ -29,33 +29,69 @@
         private void InitializeComponent()
         {
             this.btn_create = new System.Windows.Forms.Button();
+            this.txt_dstDir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_create
             // 
-            this.btn_create.Location = new System.Drawing.Point(86, 103);
+            this.btn_create.Location = new System.Drawing.Point(112, 112);
             this.btn_create.Name = "btn_create";
-            this.btn_create.Size = new System.Drawing.Size(126, 117);
+            this.btn_create.Size = new System.Drawing.Size(149, 49);
             this.btn_create.TabIndex = 0;
             this.btn_create.Text = "Create";
             this.btn_create.UseVisualStyleBackColor = true;
             this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
             // 
+            // txt_dstDir
+            // 
+            this.txt_dstDir.Location = new System.Drawing.Point(78, 49);
+            this.txt_dstDir.Name = "txt_dstDir";
+            this.txt_dstDir.Size = new System.Drawing.Size(220, 20);
+            this.txt_dstDir.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Destination:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(37, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(321, 307);
+            this.ClientSize = new System.Drawing.Size(375, 175);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_dstDir);
             this.Controls.Add(this.btn_create);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn_create;
+        private System.Windows.Forms.TextBox txt_dstDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
